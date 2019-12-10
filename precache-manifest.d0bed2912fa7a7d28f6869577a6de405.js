cef36d542e40bcf66d9f49b73f411e8b9e9dc310
Mark Armanious <markarmanus@gmail.com>
2019-12-10 00:29:50 -0330

    Updates

--- precache-manifest.d0bed2912fa7a7d28f6869577a6de405.js
+++ precache-manifest.d0bed2912fa7a7d28f6869577a6de405.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "16c215ea73709bb463d606c878f351a8",
+    "revision": "0f206b4abf2e081b70410a5c58ea967f",
     "url": "//markarmanus.github.io/path_finder/index.html"
   },
   {
-    "revision": "d7623e584231ccae34ef",
+    "revision": "1cce8c39e4e86fdaa951",
     "url": "//markarmanus.github.io/path_finder/static/css/2.fb9ed26a.chunk.css"
   },
   {
-    "revision": "1616ab8be7886c1e3d52",
+    "revision": "c2d1ff382bac13148c64",
     "url": "//markarmanus.github.io/path_finder/static/css/main.eda72dcc.chunk.css"
   },
   {
-    "revision": "d7623e584231ccae34ef",
-    "url": "//markarmanus.github.io/path_finder/static/js/2.c0dbd7f2.chunk.js"
+    "revision": "1cce8c39e4e86fdaa951",
+    "url": "//markarmanus.github.io/path_finder/static/js/2.2b91ae69.chunk.js"
   },
   {
-    "revision": "1616ab8be7886c1e3d52",
-    "url": "//markarmanus.github.io/path_finder/static/js/main.d38906b6.chunk.js"
+    "revision": "c2d1ff382bac13148c64",
+    "url": "//markarmanus.github.io/path_finder/static/js/main.8554bed6.chunk.js"
   },
   {
     "revision": "e67525fadf8d6c69b754",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//markarmanus.github.io/path_finder/precache-manifest.88f858d8c6cec27fc66d0b2e9430c735.js"
+  "//markarmanus.github.io/path_finder/precache-manifest.d0bed2912fa7a7d28f6869577a6de405.js"
 );
 
 self.addEventListener('message', (event) => {
